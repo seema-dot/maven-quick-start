2e0da2467e9825360750be8dba10d24d7c3fc1c5
seema <ms.heer3003@gmail.com>
2020-09-01 18:27:38 -0700

    Added dEpendencies

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,8 +2,15 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
     
+	
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words,' ');
+		return(separateWords == null)?0:separateWords.length;
+	}
 	public void greet(){
 		List<String>greetings = new ArrayList<>();
 		greetings.add("Hello");
@@ -22,5 +29,6 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
 	app.greet();
+	System.out.println("Word Count is:"+app.countWords("I have five words"));
     }
 }
